4a5c740a8d4b0bb830490c5283ff9ffe12833f6e
Michael Teagle <mkteagle@gmail.com>
2024-12-10 12:47:30 -0700

    adding correct link

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -99,7 +99,7 @@ export function Sidebar() {
                 className="w-full text-slate-400 hover:text-white hover:bg-slate-800/30 transition-all duration-200"
               >
                 <Link
-                  href="/admin"
+                  href="/admin/dashboard"
                   className="flex items-center gap-3 px-4 py-2"
                 >
                   <Shield className="w-5 h-5" />
